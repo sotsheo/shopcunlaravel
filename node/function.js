0ec1220f24888eaafe13482aeab0af8984a9acd0
Thanh <thanhnv.k59httt@gmail.com>
2018-02-25 23:11:10 +0700

    Lan 1 commit

---
+++ node/function.js
@@ -0,0 +1,18 @@
+// cah goi 1 
+	// function ham(){
+	// 	console.log("Cach goi ham 1");
+	// }
+	// ham();
+// cach goi ham 2 
+	// function ham(){
+	// 	console.log("Cach goi ham 2");
+	// }
+	// function goiham(fn){
+	// 	fn();
+	// }
+	// goiham(ham);
+// cah goi ham 3
+var bien=function(){
+		console.log("Cach goi ham 3");
+	}
+bien();
